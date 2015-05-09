6955b691a0e2e68dbae5e2490e79ea2945e9afa5
pemrouz <p3pedram@hotmail.com>
2015-05-09 18:38:25 +0100

    fail gracefully if no resource and break down tests

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var has = require('has')
 module.exports = function header(header, value) {
   var getter = arguments.length == 1
   return function(d){ 
-    return !has(d, 'headers')      ? null
+    return !d                      ? null
+         : !has(d, 'headers')      ? null
          : !has(d.headers, header) ? null
          : getter                  ? d['headers'][header]
                                    : d['headers'][header] == value
--- test.js
+++ test.js
@@ -1,14 +1,31 @@
 var expect = require('chai').expect
   , header = require('./')
+  , o = { body: [], headers: { 'cache': 'no-store' } }
 
 describe('header', function() {
-  it('should get a header value', function() {
-    var o = { body: [], headers: { 'cache': 'no-store' } }
-    expect(header('cache')(o)).to.equal('no-store')
-    expect(header('cache', 'no-store')(o)).to.be.ok
-    expect(header('cache', '')(o)).to.not.be.ok
-    expect(header('content-type')(o)).to.not.be.ok
-    expect(header('content-type')({})).to.not.be.ok
-    expect(header('content-type')({ headers: {} })).to.not.be.ok
+  
+  it('should return gracefully if no resource', function() {
+    expect(header('cache')()).to.not.be.ok
   })
+
+  it('should return gracefully if no headers', function() {
+    expect(header('content-type')({})).to.not.be.ok
+  })
+
+  it('should return gracefully if no header', function() {
+    expect(header('content-type')(o)).to.not.be.ok
+  })
+
+  it('should return false if key==value check fails', function() {
+    expect(header('cache', '')(o)).to.be.false
+  })
+
+  it('should return true if key==value check passes', function() {
+    expect(header('cache', 'no-store')(o)).to.be.true
+  })
+
+  it('should get a header value', function() {
+    expect(header('cache')(o)).to.equal('no-store')
+  })
+
 })
